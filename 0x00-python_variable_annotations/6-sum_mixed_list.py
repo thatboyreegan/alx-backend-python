6d167e003a75aff9ee6806437e81d6ce0810eb38
Reagan Ronald <reaganronald491@gmail.com>
2023-10-06 03:46:31 +0300

    imported typing.Callable

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,7 +3,7 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """takes a list and returns sum of elements as float
 
     Args:
--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """8-make_multiplier: has function make_multiplier"""
+from typing import Callable
 
 
-def make_multiplier(multiplier: float) -> callable[[float], float]:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """returns a function that multiplies the float wit a multiplier"""
     def multiplier_(num: float) -> float:
         """multiplies the float with a multiplier"""
